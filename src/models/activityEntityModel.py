2f5cb35fa607f79e04e6d988e23a9fe90f48380d
Rômulo Soares <romulosoares@ice.ufjf.br>
2022-03-07 14:48:16 -0300

    Create model for recive activity and entity data

---
+++ src/models/activityEntityModel.py
@@ -0,0 +1,54 @@
+from pydantic import BaseModel, Field
+from typing import Optional
+
+from src.models.activityModel import ActivityModel
+from src.models.entityModel import EntityModel
+
+class ActivityEntityModel(BaseModel):
+   activity: ActivityModel = Field(...)
+   entity: EntityModel = Field(...)
+   
+   class Config:
+      allow_population_by_field_name = True
+      arbitrary_types_allowed = True
+      schema_extra = {
+         "example": {
+            'activity': {
+               'name': 'Activity Name',
+               'provType': 'activity',
+               'start_time': 'time1',
+               'end_time': 'time2'
+            },
+            'entity': {
+               "name": "Entity Name",
+               "provType": "activity" 
+            },         
+         }
+      }
+   
+   def __iter__(self):
+      yield 'name', self.name
+      yield 'provType', self.provType
+      yield 'start_time', self.start_time
+      yield 'end_time', self.end_time
+      
+class UpdateActivityModel(BaseModel):
+   activity: Optional[ActivityModel]
+   entity: Optional[EntityModel]
+   
+   class Config:
+      arbitrary_types_allowed = True
+      schema_extra = {
+         "example": {
+            'activity': {
+               'name': 'Activity Name',
+               'provType': 'activity',
+               'start_time': 'time1',
+               'end_time': 'time3'
+            },
+            'entity': {
+               "name": "Entity Name",
+               "provType": "activity" 
+            },         
+         }    
+      }
