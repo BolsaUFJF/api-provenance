2fa1d1be2eb0a99c7327e153d4f904b547e033aa
Rômulo Soares <romulosoares@ice.ufjf.br>
2022-03-07 14:47:39 -0300

    Create model for entity data

---
+++ src/models/entityModel.py
@@ -0,0 +1,33 @@
+from pydantic import BaseModel, Field
+from typing import Optional
+
+class EntityModel(BaseModel):
+   name: str = Field(...)
+   provType: str = Field(...)
+   
+   class Config:
+      allow_population_by_field_name = True
+      arbitrary_types_allowed = True
+      schema_extra = {
+         "example": {
+            "name": "Entity Name",
+            "provType": "activity"    
+         }
+      }
+   
+   def __iter__(self):
+      yield 'name', self.name
+      yield 'provType', self.provType
+      
+class UpdateEntityModel(BaseModel):
+   name: Optional[str]
+   provType: Optional[str]
+   
+   class Config:
+      arbitrary_types_allowed = True
+      schema_extra = {
+         "example": {
+            'name': 'Entity Name',
+            'provType': 'entity-type'       
+         }
+      }
