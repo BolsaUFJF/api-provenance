e7ae931461c09e05111a0d0b268474bd3bb19bfc
Rômulo Soares <romulosoares@ice.ufjf.br>
2022-03-08 08:56:15 -0300

    Create route to create 'was associated with' relationship

--- app.py
+++ app.py
@@ -5,11 +5,13 @@ from src.database.databaseNeo4j import neo4j_driver
 
 from src.routes.wasUsedRoutes import router as was_used_routes
 from src.routes.wasGeneratedBy import router as was_generated_by_routes
+from src.routes.wasAssociatedWith import router as was_associated_with_routes
 
 app = FastAPI()
 
 app.include_router(was_used_routes)
 app.include_router(was_generated_by_routes)
+app.include_router(was_associated_with_routes)
 
 @app.get('/', response_description="Root")
 def root():
