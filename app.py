4ef60267d4a716e9b16c515e5f74a6b8838c1319
Rômulo Soares <romulosoares@ice.ufjf.br>
2022-03-08 08:40:03 -0300

    Create route to create 'was generated by' relationship

--- app.py
+++ app.py
@@ -4,10 +4,12 @@ from typing import Optional
 from src.database.databaseNeo4j import neo4j_driver
 
 from src.routes.wasUsedRoutes import router as was_used_routes
+from src.routes.wasGeneratedBy import router as was_generated_by_routes
 
 app = FastAPI()
 
 app.include_router(was_used_routes)
+app.include_router(was_generated_by_routes)
 
 @app.get('/', response_description="Root")
 def root():
