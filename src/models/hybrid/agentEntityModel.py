a5469e50ce5e0f7d19388b36d99b8adbed787384
Rômulo Soares <romulosoares@ice.ufjf.br>
2022-03-08 08:23:47 -0300

    Create model for recive agent and entity data

---
+++ src/models/hybrid/agentEntityModel.py
@@ -0,0 +1,52 @@
+from pydantic import BaseModel, Field
+from typing import Optional
+
+from models.provenance.activityModel import ActivityModel
+from models.provenance.entityModel import EntityModel
+from models.provenance.agentModel import AgentModel
+
+class AgentEntityModel(BaseModel):
+   agent: AgentModel = Field(...)
+   entity: EntityModel = Field(...)
+   
+   class Config:
+      allow_population_by_field_name = True
+      arbitrary_types_allowed = True
+      schema_extra = {
+         "example": {
+            'agent': {
+               "name": "Agent Name",
+               "provType": "agent-type",
+               "data": {}  
+            },
+            'entity': {
+               "name": "Entity Name",
+               "provType": "activity" 
+            },         
+         }
+      }
+   
+   def __iter__(self):
+      yield 'agent', self.agent
+      yield 'entity', self.entity
+      
+class UpdateAgentEntityModel(BaseModel):
+   agent: Optional[AgentModel]
+   entity: Optional[EntityModel]
+   
+   class Config:
+      arbitrary_types_allowed = True
+      schema_extra = {
+         "example": {
+            'agent1': {
+               "name": "Agent Name",
+               "provType": "agent-type",
+               "data": {}  
+            },
+            'agent2': {
+               "name": "Agent Name",
+               "provType": "agent-type",
+               "data": {} 
+            },         
+         }  
+      }
