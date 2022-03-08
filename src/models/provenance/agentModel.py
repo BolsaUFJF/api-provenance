3e610c3799edb46ca4bc681a9b56a971faafe0ac
Rômulo Soares <romulosoares@ice.ufjf.br>
2022-03-08 08:01:04 -0300

    Create model for agent data

--- src/models/provenance/agentModel.py
+++ src/models/provenance/agentModel.py
@@ -4,30 +4,35 @@ from typing import Optional
 class EntityModel(BaseModel):
    name: str = Field(...)
    provType: str = Field(...)
+   pki: dict = Field(...)
    
    class Config:
       allow_population_by_field_name = True
       arbitrary_types_allowed = True
       schema_extra = {
          "example": {
-            "name": "Entity Name",
-            "provType": "entity-type"    
+            "name": "Agent Name",
+            "provType": "agent-type",
+            "data": {}   
          }
       }
    
    def __iter__(self):
       yield 'name', self.name
       yield 'provType', self.provType
+      yield 'data', self.data
       
 class UpdateEntityModel(BaseModel):
    name: Optional[str]
    provType: Optional[str]
+   pki: Optional[str]
    
    class Config:
       arbitrary_types_allowed = True
       schema_extra = {
          "example": {
-            'name': 'Entity Name',
-            'provType': 'entity-type'       
+            "name": "Agent Name",
+            "provType": "agent-type",
+            "pki": {}    
          }
       }
