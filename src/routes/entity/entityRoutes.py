d9b0836bd0f558593fe289da227b7197b6329038
Rômulo Soares <romulosoares@ice.ufjf.br>
2022-03-16 10:23:18 -0300

    Created route to get entity by name

--- src/routes/entity/entityRoutes.py
+++ src/routes/entity/entityRoutes.py
@@ -26,3 +26,19 @@ async def get_last_entity():
       resultData = result.data()[-1]
    
    return resultData
+
+@router.get('/get-by-name/{name}', response_description="Get Last Entity")
+async def get_by_name(name: str):
+
+   query = (
+      "MATCH (n:Entity) "
+      "WHERE n.name = $name "
+      "RETURN n "
+      "LIMIT 1 "
+   )
+   
+   with neo4j_driver.session() as session:      
+      result = session.run(query, name=name)
+      resultData = result.data()[0]
+   
+   return resultData
